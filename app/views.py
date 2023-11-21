d3ef4001a021da4e46d545b554fcaea1a5d2569b
gurulakshmikanth <gurukanth121.com>
2023-11-21 12:14:57 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def srujana(request):
+    return render(request,'srujana.html')
+
+def sudher(request):
+    return render(request,'sudher.html')
